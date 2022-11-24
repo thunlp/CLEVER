5732e31b5fb3ade35ae41e22bc487fce994a8076
yiranyyu <2606375857@qq.com>
2022-11-24 15:40:38 +0800

    Add Implementation of CLEVER; Update Download Instruction

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,6 @@
+# Download meta file
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_vg_dict.json -O vg_dict.json
+
 # Download bag split
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_val_bag_data.json -O val_bag_data.json
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_test_bag_data.json -O test_bag_data.json
@@ -8,11 +11,24 @@ wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_val_pairs_data.jso
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_test_pairs_data.json -O test_pairs_data.json
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_train_pairs_data.json -O train_pairs_data.json
 
+# Download bag image sampling cache
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_val_selected_image_of_bags.pkl -O val_image_sort_by_IoU_and_distance.pkl
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_test_selected_image_of_bags.pkl -O test_image_sort_by_IoU_and_distance.pkl
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_train_selected_image_of_bags.pkl -O train_image_sort_by_IoU_and_distance.pkl
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_val_image_idx_to_id.pkl -O val_image_idx_to_id.pkl
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_test_image_idx_to_id.pkl -O test_image_idx_to_id.pkl
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_train_image_idx_to_id.pkl -O train_image_idx_to_id.pkl
+
 
 # Download feature file
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_val_obj_feat.json -O obj_feat_val.tsv
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_test_obj_feat.json -O obj_feat_test.tsv
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_train_obj_feat.json -O obj_feat_train.tsv
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_val_feat_idx_to_label_line.tsv -O val_feat_idx_to_label_line.tsv
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_test_feat_idx_to_label_line.tsv -O test_feat_idx_to_label_line.tsv
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_train_feat_idx_to_label_line.tsv -O train_feat_idx_to_label_line.tsv
+
 
 # Download label file
 wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_AAAI23_VG100-100_label.tsv -O VG_100_100_label.tsv
+
---
+++ data/download_weight.sh
@@ -0,0 +1,6 @@
+# Download pretrained weight
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_pretrained_vinvl.tar.gz -O pretrained_vinvl.tar.gz
+wget https://thunlp.oss-cn-qingdao.aliyuncs.com/CLEVER_CKE_pretrained_avg.tar.gz -O pretrained_avg_model.tar.gz
+
+tar -xvf pretrained_vinvl.tar.gz
+tar -xvf pretrained_avg_model.tar.gz
